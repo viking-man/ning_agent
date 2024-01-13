7ae38977952af205c28d18b6ea788d08d0a62a90
weiking <jiangwei4953267@163.com>
2024-01-13 19:19:45 +0800

    outline v1.0

--- app/agent_openai/agent_facade.py
+++ app/agent_openai/agent_facade.py
@@ -3,3 +3,7 @@ from agent import agent_core
 def dispatch(chat:str="",chat_history:str=""):
    content =  agent_core.query(chat,chat_history)
    return content
+
+if __name__ == "__main__":
+    
+    dispatch("文明的起源")
