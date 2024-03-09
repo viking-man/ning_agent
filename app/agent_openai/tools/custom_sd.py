95a418d1809be27c7a63cbf67b802f7ee29c64bc
viking <jiangwei4953267@163.com>
2024-03-09 16:20:41 +0800

    outlinev1.0

--- app/agent_openai/tools/custom_sd.py
+++ app/agent_openai/tools/custom_sd.py
@@ -4,6 +4,8 @@ from langchain.agents import tool
 from torch import torch
 import time
 from pathlib import Path
+from app.common.utils import string_utils
+from translatepy import Translate
 
 
 class CustomStableDiffusion:
@@ -22,9 +24,18 @@ sd = CustomStableDiffusion()
 '''该方法用于用户绘画相关的需求，提供根据文本生成图片的能力，用户输入图片相关的指令描述，该方法返回对应的图片输出'''
 
 
+def translate_text(image_text, target_language='zh-CN'):
+    translator = Translate()
+    translated_text = translator.translate(image_text, target_language).result
+    return translated_text
+
+
 @tool
 def sculpture(prompt: str = ""):
     '''This method is used for user drawing-related needs, providing the ability to generate images based on the text, the user inputs a description of the image-related instructions, the method returns the corresponding image output'''
+
+    if string_utils.contains_chinese(prompt):
+        prompt = translate_text(prompt, target_language="en")
 
     image = sd.pipeline(prompt).images[0]
 
